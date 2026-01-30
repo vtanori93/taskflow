1d2f3c3bd2385a87e26952a30992a588f581135c
vtanori93 <vtanori93@gmail.com>
2026-01-30 12:15:24 -0700

    feat: add splash screen

--- mobileapp/src/screens/SplashScreen.tsx
+++ mobileapp/src/screens/SplashScreen.tsx
@@ -1,14 +1,15 @@
 import { useNavigation } from '@react-navigation/native';
 import React, { useEffect } from 'react';
-import { Text, View } from 'react-native';
-import { Session } from '../services/session';
+import { Image, View } from 'react-native';
 import api from '../services/api';
+import { Session } from '../services/session';
 
 export default function SplashScreen() {
   const navigation = useNavigation();
   useEffect(() => {
     let isActive = true;
     const checkSession = async () => {
+      await new Promise(res => setTimeout(res, 5000)); // Espera 5 segundos
       const session = await Session.getSession();
       if (session && session.access_token) {
         // Verificar token llamando a /auth/profile
@@ -32,8 +33,11 @@ export default function SplashScreen() {
     return () => { isActive = false; };
   }, [navigation]);
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Splash Screen</Text>
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: '#fff' }}>
+      <Image
+        source={{ uri: 'https://media4.giphy.com/media/v1.Y2lkPTZjMDliOTUyZDBjamVjN3pnYmtwNjEzam1jeTRnOHVqejdobWIwMTR5Y2wyMnkyYyZlcD12MV9zdGlja2Vyc19zZWFyY2gmY3Q9cw/X0Vihujvo7FiEIXOsm/giphy.gif' }}
+        style={{ width: 180, height: 180, resizeMode: 'contain' }}
+      />
     </View>
   );
 }
